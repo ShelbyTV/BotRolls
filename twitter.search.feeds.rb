86c0d268b8b87968e08d33596efb9dca68419495
Henry Sztul <henry@sztul.com>
2012-06-04 14:09:47 -0400

    added embedly regex from gt to check if a vid is potentially supported and added a syria video roll

--- twitter.search.feeds.rb
+++ twitter.search.feeds.rb
@@ -1,26 +1,42 @@
 #!/usr/bin/env ruby
+
+###########################################
+# Dependencies
 require "redis"
 require "grackle"
 require "open-uri"
 require "yaml"
+require "json"
 
+load 'embedly_regexes.rb'
+
+###########################################
+# Loading the config file w urls/search terms/twitter acct info 
 #config = YAML.load( File.read("/home/gt/utils/VideoFeedRss/feeds.yml") )
+config = YAML.load( File.read("feeds.yml") )
 
+
+###########################################
 # the twitter seach term:
+
 search_term = URI.escape(ARGV[0])
 rpp = ARGV[1].to_i || 20
+
 search_client = Grackle::Client.new
-=begin
-service_config = config["defaults"][service]
+
+service_config = config["defaults"][search_term]
 (puts "invalid service"; exit) unless service_config
 
 search_term = service_config["search_term"]
 token = service_config["tw_token"]
 secret = service_config["tw_secret"]
 
+###########################################
 # Grackle used for tweeting found videos for ingestion into shelby
 # [this is shelby's key/secret pair]
+# NOTE: eventually this should be done via the Shelby API
 (puts "must include twitter credentials"; exit) unless (token and secret)
+
 consumer_key = config["defaults"]["consumer_app"]["consumer_key"]
 consumer_secret = config["defaults"]["consumer_app"]["consumer_secret"]
 tw_client = Grackle::Client.new(:auth=>{
@@ -29,37 +45,38 @@ tw_client = Grackle::Client.new(:auth=>{
   :token => token,
   :token_secret => secret
 })
-=end
+
+###########################################
 # Redis used for persisting last know video
-#redis = Redis.new
-#redis_key = "last_#{search_term.join('_')}_video_time"
+redis = Redis.new
+redis_key = "last_#{search_term}_video_id"
+###########################################
+###########################################
 
 # getting pubDate of last known video in feed
-#last_old_video_time = redis.get redis_key
+last_old_video_id = redis.get redis_key
 
 # do twitter search:
-search_result = search_client[:search].search? :q => search_term, :include_entities => true, :rpp => rpp
+search_result = search_client[:search].search? :q => search_term, :include_entities => true, :rpp => rpp, :since_id => last_old_video_id
 
 begin
-  # TODO:
-  #first_new_video_id = 
-  #if last_old_video_id != first_new_video_id
-    search_result.results.each do |r|
-      #break if r.id == last_old_video_id
-      puts r.text if r.entities.urls
-      #vid = { "title" => r.title,
-      #        "link" => r.link,
-      #        "timestamp" => r.time } 
-              # FIX ^
-
+  redis.set redis_key, search_result.max_id
+  
+  search_result.results.each do |r|
+    if r.entities.urls.length > 0 and Embedly::Regexes.video_regexes_matches?(r.entities.urls.first.expanded_url)      
       # Send vids to shelbz, for now via tweet (<140 char. duh)
-      #tweet = "#{vid['title'][0..90]}... #{vid['link']}"
-      #tw_client.statuses.update! :status=> tweet
-      #puts tweet
-      #sleep 1
+      begin
+        #tw_client.statuses.retweet!(:id => r.id) if r.entities.urls.length > 0
+      rescue => e
+        puts "[#{Time.now}] [#{search_term.swapcase} GRACKLE ERROR]: #{e}"
+      end
+      
+      
+      puts r.entities.urls.first.expanded_url
+      
+      sleep 0.5
     end
-    #redis.set redis_key, first_new_video_id
-  #end
+  end
 rescue => e
-  puts "[#{Time.now}] [#{search_term.swapcase} VIDEO FEED ERROR]: #{e}"
+  puts "[#{Time.now}] [#{search_term.swapcase} ERROR]: #{e}"
 end
