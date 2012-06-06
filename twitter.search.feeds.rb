8d1a8d3035cbb7e514fe966d0df892600941b407
Henry Sztul <henry@sztul.com>
2012-06-06 12:21:28 -0400

    pagin through search results

--- twitter.search.feeds.rb
+++ twitter.search.feeds.rb
@@ -17,7 +17,7 @@ config = YAML.load( File.read("feeds.yml") )
 
 
 ###########################################
-# the twitter seach term:
+# the twitter seach term that only returns results with links:
 
 search_term = URI.escape(ARGV[0] + " filter:links")
 rpp = ARGV[1].to_i || 20
@@ -49,38 +49,52 @@ tw_client = Grackle::Client.new(:auth=>{
 ###########################################
 # Redis used for persisting last know video
 redis = Redis.new
-redis_key = "last_#{search_term}_video_id"
+redis_key = "last_#{search_term}_tweet_id"
 ###########################################
 ###########################################
 
-# getting pubDate of last known video in feed
+# getting id of last known tweet
 last_old_video_id = redis.get redis_key
 
 # do twitter search:
-search_result = search_client[:search].search? :q => search_term, :include_entities => true, :rpp => rpp, :since_id => last_old_video_id
+page = 1
+search_result = search_client[:search].search? :q => search_term, 
+                    :include_entities => true, 
+                    :rpp => rpp, 
+                    :since_id => last_old_video_id,
+                    :page => page
+
+# storing the max_id in redis
+redis.set redis_key, search_result.max_id
 
 begin
-  redis.set redis_key, search_result.max_id
+  # this loops through all of the tweets since last sweep
+  while search_result.results.length > 0 and page < 30 # lit on page is just trying to limit time it takes to run this
   
-  search_result.results.each do |r|
-    if r.entities.urls.length > 0 and Embedly::Regexes.video_regexes_matches?(r.entities.urls.first.expanded_url)      
+    search_result.results.each do |r|
+      if r.entities.urls.length > 0 and Embedly::Regexes.video_regexes_matches?(r.entities.urls.first.expanded_url)      
 
-      # Send vids to shelbz, for now via tweet (<140 char. duh)
-      # NOTE: eventually this should be done via the Shelby API
-      begin
-        tw_client.statuses.retweet!(:id => r.id) if r.entities.urls.length > 0
-      rescue => e
-        puts "[#{Time.now}] [#{search_term.swapcase} GRACKLE ERROR]: #{e}"
+        # Send vids to shelbz, for now via tweet (<140 char. duh)
+        # NOTE: eventually this should be done via the Shelby API
+        begin
+          tw_client.statuses.retweet!(:id => r.id) if r.entities.urls.length > 0
+        rescue => e
+          puts "[#{Time.now}] [#{search_term.swapcase} GRACKLE ERROR]: #{e}"
+        end
+      
+        sleep 0.5
       end
-      
-      puts r.entities.urls.first.expanded_url
-      
-      sleep 0.5
-    else
-      #puts r.text
-      print "."
     end
+    
+    page += 1
+        
+    search_result = search_client[:search].search? :q => search_term, 
+                        :include_entities => true, 
+                        :rpp => rpp, 
+                        :since_id => last_old_video_id,
+                        :page => page
   end
+  
 rescue => e
   puts "[#{Time.now}] [#{search_term.swapcase} ERROR]: #{e}"
 end
