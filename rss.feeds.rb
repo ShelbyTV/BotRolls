60434621d940b052c40543a0a91891470bd47587
Henry Sztul <henry@sztul.com>
2012-11-03 09:11:55 -0400

    getting botrolls in a good place, no dupes

--- rss.feeds.rb
+++ rss.feeds.rb
@@ -19,24 +19,8 @@ service_config = config["defaults"][service]
 (puts "invalid service"; exit) unless service_config
 
 feed_url = service_config["feed_url"]
-token = service_config["tw_token"]
-secret = service_config["tw_secret"]
 shelby_token = service_config["shelby_auth_token"]
 shelby_roll_id = service_config["roll_id"]
-
-
-# Grackle used for tweeting found videos for ingestion into shelby
-# [this is shelby's key/secret pair]
-if (token and secret)
-  consumer_key = config["defaults"]["consumer_app"]["consumer_key"]
-  consumer_secret = config["defaults"]["consumer_app"]["consumer_secret"]
-  tw_client = Grackle::Client.new(:auth=>{
-    :type =>:oauth,
-    :consumer_key => consumer_key, :consumer_secret => consumer_secret,
-    :token => token,
-    :token_secret => secret
-  })
-end
 
 # Redis used for persisting last know video
 redis = Redis.new
@@ -50,25 +34,22 @@ begin
   key = redis.get redis_key
   last_old_video_time = key == "" ? "" : Time.parse(key)
   
-  first_new_video_time = feed.xpath('//channel/item').first.xpath('pubDate').inner_text
   feed.xpath('//channel/item').reverse.each do |i|
-    break if last_old_video_time.is_a?(Time) and Time.parse(i.xpath('pubDate').inner_text) >= last_old_video_time
+    break if (last_old_video_time.is_a?(Time) and Time.parse(i.xpath('pubDate').inner_text) <= last_old_video_time)
     
     vid = { "title" => i.xpath('title').inner_text,
             "link" => i.xpath('link').inner_text,
             "pubDate" => i.xpath('pubDate').inner_text }
 
     # Send vids to shelbz
-    tweet = "#{vid['title'][0..90]}... #{i.xpath('link').inner_text}"
-    if token and secret
-      tw_client.statuses.update! :status=> tweet
-    elsif shelby_roll_id and shelby_token
-      r = Shelby::API.create_frame(shelby_roll_id, shelby_token, vid['link'], tweet)
+    description = "#{vid['title'][0..90]}... #{i.xpath('link').inner_text}"
+    if shelby_roll_id and shelby_token
+      r = Shelby::API.create_frame(shelby_roll_id, shelby_token, vid['link'], description)
     end
-    puts tweet
+    redis.set redis_key, i.xpath('pubDate').inner_text
+    puts description
     sleep 1
   end
-  redis.set redis_key, first_new_video_time
 rescue => e
   puts "[#{Time.now}] [#{service.swapcase} VIDEO FEED ERROR]: #{e}"
 end
