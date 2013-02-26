0d0e84017fb09e4b32c8b1f70f6241b187b87b88
Henry <henry@sztul.com>
2013-02-26 14:28:33 -0500

    making production redis ready. prepping a test config file

--- youtube.feed.rb
+++ youtube.feed.rb
@@ -23,20 +23,20 @@ shelby_token = service_config["shelby_auth_token"]
 shelby_roll_id = service_config["roll_id"]
 
 # Redis used for persisting last know video
-#redis = Redis.new
-#redis_key = "last_#{youtube_user}_video_time"
+redis = Redis.new
+redis_key = "last_#{youtube_user}_video_time"
 
 # This is the feed with the feeds latest videos (json)
 feed_url = "http://gdata.youtube.com/feeds/api/users/" + youtube_user + "/uploads?v=2&alt=json"
 
 begin
   # getting pub_date of last known video in feed
-  #pub_date_string = redis.get redis_key
+  pub_date_string = redis.get redis_key
 
   feed = JSON.parse(open(feed_url).read)
   entries = feed["feed"]["entry"]
   entries.reverse.each do |v|
-    #last_known_video_time = (pub_date_string =="" or pub_date_string.nil?) ? "" : Time.parse(pub_date_string)
+    last_known_video_time = (pub_date_string =="" or pub_date_string.nil?) ? "" : Time.parse(pub_date_string)
 
     video = Hash.new
     video[:title] = v['title']['$t'] if v['title']
@@ -45,10 +45,10 @@ begin
     video[:pub_date] = Time.parse(v['media$group']['yt$uploaded']['$t']) if v['media$group'] and v['media$group']['yt$uploaded']
 
     #move on if we have seen this pub_date before
-    #next if (last_known_video_time.is_a?(Time) and (video[:pub_date] <= last_known_video_time))
+    next if (last_known_video_time.is_a?(Time) and (video[:pub_date] <= last_known_video_time))
 
-    if video[:url] #and shelby_roll_id and shelby_token
-      #r = Shelby::API.create_frame(shelby_roll_id, shelby_token, video[:url], video[:description])
+    if video[:url] and shelby_roll_id and shelby_token
+      r = Shelby::API.create_frame(shelby_roll_id, shelby_token, video[:url], video[:description])
       puts "added: #{video[:pub_date]}"
     end
 
@@ -56,7 +56,7 @@ begin
 
   # setting pub_date of latest video
   if entries.first['media$group'] and entries.first['media$group']['yt$uploaded']
-    #redis.set(redis_key, entries.first['media$group']['yt$uploaded']['$t'])
+    redis.set(redis_key, entries.first['media$group']['yt$uploaded']['$t'])
   end
 rescue => e
   puts "[ YT FEED ERROR ] #{youtube_user} : #{e}"
